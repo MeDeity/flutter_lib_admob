83b144f2e9a3a2af2b1b3e678ac053a6029d8e96
fwenhua <langrenbule@gmail.com>
2019-10-21 18:25:40 +0800

    update export files

---
+++ lib/admob/index_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class IndexPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return IndexPageState();
+  }
+
+}
+
+class IndexPageState extends State<IndexPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
--- lib/flutter_lib_admob.dart
+++ lib/flutter_lib_admob.dart
@@ -1,3 +1,5 @@
 library flutter_lib_admob;
 
 export 'package:flutter_lib_admob/admob/generate_admob_id.dart';
+export 'package:flutter_lib_admob/admob/ad_type_enum.dart';
+export 'package:flutter_lib_admob/admob/admob_utils.dart';
